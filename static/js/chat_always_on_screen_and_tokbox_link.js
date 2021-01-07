dc0427c34da03750dfb171759acc6502ea262ff0
John McLear <john@mclear.co.uk>
2021-01-07 22:15:39 +0000

    autofixes from Etherpad checkPlugins.js

--- static/js/chat_always_on_screen_and_tokbox_link.js
+++ static/js/chat_always_on_screen_and_tokbox_link.js
@@ -1,39 +1,38 @@
 var chat_always_on_screen_and_tokbox_link = {
 
-  getURLParameter: function(name) {
-      return decodeURI(
-          (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
-      );
+  getURLParameter(name) {
+    return decodeURI(
+        (RegExp(`${name}=` + '(.+?)(&|$)').exec(location.search) || [, null])[1]
+    );
   },
 
-  init: function() {
-
+  init() {
     chat.stickToScreen(true);
-    $("#chatinput").height("90px");
-    $("#chattext").css("bottom", "100px");
-    $("#chatinputbox > form").html("");
-    var textarea = '<textarea id="chatinput" style="width:185px;height:90px;padding-left:2px;font-size:12px;padding-top:2px;" placeholder="type here to chat..."></textarea>';
-    $("#chatinputbox > form").html(textarea);
-    $("#chatinput").keyup(function(e){
-      if(e.which == 13){
+    $('#chatinput').height('90px');
+    $('#chattext').css('bottom', '100px');
+    $('#chatinputbox > form').html('');
+    const textarea = '<textarea id="chatinput" style="width:185px;height:90px;padding-left:2px;font-size:12px;padding-top:2px;" placeholder="type here to chat..."></textarea>';
+    $('#chatinputbox > form').html(textarea);
+    $('#chatinput').keyup((e) => {
+      if (e.which == 13) {
         chat.send();
       }
     });
 
-    var tokbox_link = clientVars.tokbox_link;
-    if(tokbox_link){
-      if (chat_always_on_screen_and_tokbox_link.getURLParameter("av")){  // is it set?
-        if(chat_always_on_screen_and_tokbox_link.getURLParameter("av") === "YES"){
-          var link = tokbox_link + "?string="+location.href;
-          var markup = '<p id="tokboxStart" style="cursor:pointer; cursor:hand;text-align:center;text-size:12px;font-weight:bold;text-transform:uppercase;padding-bottom:8px;">START VIDEO/AUDIO CHAT</p>';
-          $(markup).insertBefore("#chatinput");
-          $("#tokboxStart").click(function(){
+    const tokbox_link = clientVars.tokbox_link;
+    if (tokbox_link) {
+      if (chat_always_on_screen_and_tokbox_link.getURLParameter('av')) { // is it set?
+        if (chat_always_on_screen_and_tokbox_link.getURLParameter('av') === 'YES') {
+          const link = `${tokbox_link}?string=${location.href}`;
+          const markup = '<p id="tokboxStart" style="cursor:pointer; cursor:hand;text-align:center;text-size:12px;font-weight:bold;text-transform:uppercase;padding-bottom:8px;">START VIDEO/AUDIO CHAT</p>';
+          $(markup).insertBefore('#chatinput');
+          $('#tokboxStart').click(() => {
             window.open(link, 'utilization', 'height=700,width=350,scrollbars=1');
           });
-  
-          $("#chattext").css("bottom", "125px");
+
+          $('#chattext').css('bottom', '125px');
         }
       }
     }
-  }
-}
+  },
+};
